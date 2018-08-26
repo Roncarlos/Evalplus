ec530df7f0befecbe290b1a34c7c4f3d82f21b58
Combo <32088155+KzeCombo@users.noreply.github.com>
2018-08-26 09:43:49 +0400

    OpérandeBinaire
    Ajout des opérandes binaires

---
+++ Mult.java
@@ -0,0 +1,12 @@
+package eval;
+
+public class Mult extends NoeudBinaire{
+	
+	public Mult(Noeud fg, Noeud fd){
+		super(fg, fd);		
+	}
+	
+	public double execute(){
+		return getfg().execute() * getfd().execute();
+	}
+}
